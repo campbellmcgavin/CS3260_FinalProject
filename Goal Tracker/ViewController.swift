68d15fb9b9fabc524e19978999a729367bb6c0b9
--replace-all <campbellmcgavin@gmail.com>
2021-07-27 19:48:52 -0600

    Initial Commit

---
+++ Goal Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goal Tracker
+//
+//  Created by Campbell McGavin on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
